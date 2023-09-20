9bb571f75401377ea380851279e2396dc69378df
rcasantos <rcadosantos@outlook.com>
2023-09-20 02:16:20 -0300

    refactor: change app config to use httpserver

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,8 +2,10 @@ import 'dotenv/config'
 import 'reflect-metadata'
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import http from './http'
 
 const app = express()
+const httpServer = http(app)
 setupMiddlewares(app)
 
-export default app
+export default httpServer
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import app from './config/app'
+import httpServer from './config/app'
 import { AppDataSource } from './config/data-source'
 
+const bootstrap = async (): Promise<void> => {
+  await new Promise<void>((resolve) => httpServer.listen({ port: process.env.PORT }, resolve))
+  console.log(`🚀 Server ready at http://localhost:${process.env.PORT}/`)
+}
+
 void AppDataSource.initialize().then(() => {
-  app.listen(process.env.PORT, (): void => {
-    console.log(`Server running at http://localhost:${process.env.PORT}`)
-  })
+  void bootstrap()
 })

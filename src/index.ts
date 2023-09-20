6c4b588207990eaf393e45de3b4482c72f5c7a50
rcasantos <rcadosantos@outlook.com>
2023-09-20 01:18:16 -0300

    feat: create content type middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config'
 import express from 'express'
-import { bodyParser } from './middlewares'
+import { bodyParser, contentType } from './middlewares'
 
 const app = express()
 app.use(bodyParser)
+app.use(contentType)
 
 app.listen(process.env.PORT, (): void => {
   console.log(`Server running at http://localhost:${process.env.PORT}`)

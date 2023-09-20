10139c2caa966df9b6c300f4b5b9d77520ce4cea
rcasantos <rcadosantos@outlook.com>
2023-09-20 01:11:49 -0300

    chore: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
 
-app.listen(4000, (): void => {
-  console.log(`Server running at http://localhost:${4000}`)
+app.listen(process.env.PORT, (): void => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`)
 })

e0b0f66d1d945aa2822095c716f5be1f66a124cc
rcasantos <rcadosantos@outlook.com>
2023-09-20 01:15:14 -0300

    feat: create body parser middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import 'dotenv/config'
 import express from 'express'
+import { bodyParser } from './middlewares'
 
 const app = express()
+app.use(bodyParser)
 
 app.listen(process.env.PORT, (): void => {
   console.log(`Server running at http://localhost:${process.env.PORT}`)

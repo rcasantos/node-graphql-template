524eb5b55cca277aea197289c454c007a7cc89ca
rcasantos <rcadosantos@outlook.com>
2023-09-20 01:23:43 -0300

    chore: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config'
 import express from 'express'
-import { bodyParser, contentType } from './middlewares'
+import { bodyParser, contentType, cors } from './middlewares'
 
 const app = express()
 app.use(bodyParser)
 app.use(contentType)
+app.use(cors)
 
 app.listen(process.env.PORT, (): void => {
   console.log(`Server running at http://localhost:${process.env.PORT}`)
